0b37d8cc49988a24da619f014dc32c9ad888cecb
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2016-04-28 11:32:29 +0900

    Add components

---
+++ app/assets/javascripts/components/repository.js.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Repository extends React.Component {
+
+  render() {
+    return (<div>Repository</div>);
+  }
+  
+}
+
+export default Repository;
---
+++ app/assets/javascripts/components/repository_list.js.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class RepositoryList extends React.Component {
+
+  render() {
+    return (<div>RepositoryList</div>);
+  }
+  
+}
